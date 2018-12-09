9b680eea5a1e7947b2a7d36566358f6727117660
Sokthai <sokthaitang@gmail.com>
2018-12-08 23:55:18 -0500

    add dynamic board and letter to the rack

--- hw9/script/myscript.js
+++ hw9/script/myscript.js
@@ -5,7 +5,7 @@
     HW9
     Using jQuery drag and drop UI Library
     Created by ST on Dec/07/2018
-    Updateed on 
+    Updateed on 12/08/18
 */
 
 
@@ -13,11 +13,11 @@
 
 $(document).ready(function () {
 
-    
-    
-    
-    
-    
+
+
+
+
+
     generateTable();
 
     function generateTable() {
@@ -29,19 +29,197 @@ $(document).ready(function () {
             let tr = $("<tr>");
             for (j = 0; j < 15; j++) {
                 let td = $("<td>");
+                let myClass = "";
+
                 td.addClass("gth snap").text("");
-               
+                td.attr("id", getLetter(i) + j);
+                switch (i) { //set up the class for each td for set up backround image
+                    case 0:
+                    case 14:
+                        if (j === 0 || j === 7 || j === 14) {
+                            td.addClass("tw");
+                        } else if (j === 3 || j === 11) {
+                            td.addClass("dl");
+                        }
+                        break;
+                    case 1:
+                    case 13:
+                        if (j === 1 || j === 13) {
+                            td.addClass("dw");
+                        } else if (j === 5 || j === 9) {
+                            td.addClass("tl");
+                        }
+                        break;
+                    case 2:
+                    case 12:
+                        if (j === 2 || j === 12) {
+                            td.addClass("dw");
+                        } else if (j === 6 || j === 8) {
+                            td.addClass("dl");
+                        }
+                        break;
+                    case 3:
+                    case 11:
+                        if (j === 0 || j === 7 || j === 14) {
+                            td.addClass("dl");
+                        } else if (j === 3 || j === 11) {
+                            td.addClass("dw");
+                        }
+                        break;
+                    case 4:
+                    case 10:
+                        if (j === 4 || j === 10) {
+                            td.addClass("dw");
+                        }
+                        break;
+                    case 5:
+                    case 9:
+                        if (j === 1 || j === 5 || j === 9 || j === 13) {
+                            td.addClass("tl");
+                        }
+                        break;
+                    case 6:
+                    case 8:
+                        if (j === 2 || j === 6 || j === 8 || j === 12) {
+                            td.addClass("dl");
+                        }
+                        break;
+                    default:
+                        if (j === 0 || j === 14) {
+                            td.addClass("tw");
+                        } else if (j === 3 || j === 11) {
+                            td.addClass("dl");
+                        } else if (j === 7) {
+                            td.addClass("star");
+                        }
+                }
+
+
+
                 tr.append(td); //add td to tr
             }
             tbody.append(tr); //append tr to tbody
         }
         table.append(tbody); //add tbody to table
         $("#table").append(table);
+
+    }
+
+
+
+    function getLetter(i) {
+        const a = 97;
+        return String.fromCharCode(a + i);
+    }
+
+
+    function readJson(filePath) { //could read the file, but cannot save to a variable
+        $.getJSON(filePath, function (data) {
+            console.log(data);
+        });
+
+    };
+    // readJson("script/pieces.json");
+
+
+    let json = (function () { //getting letters from json file and save to a variable
+        var json = null;
+        $.ajax({
+            'async': false,
+            // 'global': false,
+            'url': "script/pieces.json",
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })();
+
+    var letters = [];
+    var availableLetter = 100; //English scrabble of 100 letters
+
+    function generateLetter() { //generate 7 letter and save to a array of objects
+        let l = [];
+        let p = (availableLetter >= (7 - letters.length)) ? 7 : letters.length + availableLetter; //if there are only 2, for example, in the bag while play need 5, give player only those last two
+        for (let i = letters.length; i < p; i++) {
+            let randomIndex = getRandomIndex();
+
+            let obj = { //save the letter and its value
+                "letter": json.pieces[randomIndex].letter,
+                "value": json.pieces[randomIndex].value
+            };
+            json.pieces[randomIndex].quantity--; //minus one after take one letter from the bag 
+            letters.push(obj);
+            availableLetter--; //minus a letter each time a letter is used
+        }
+    }
+
+    generateLetter();
+
+    function reRackLetter() {
+
+        // let mytable = $("table").attr("id", "standTable");
+        // let mytbody = $("tbody");
+        
+        //     let mytr = $("<tr>");
+        //     for (i = 0; i < 7; i++) {
+        //         let mytd = $("<td>");
+        //         mytd.addClass("snap").text("");
+        //         mytd.attr("id", getLetter(i));
+        //         mytr.append(mytd); //add td to tr
+        //     }
+        //     mytbody.append(mytr); //append tr to tbody
+        
+        // mytable.append(mytbody); //add tbody to table
+        // $("#stand").append(mytable);
+
+
+        for (let i = 0; i < letters.length; i++) { //the 7 tiles
+            
+            let image = "url('images/" + letters[i].letter + ".jpg')";
+            let tiles = $("<div>").addClass("tiles");
+            let id = "#" + getLetter(i);
+            tiles.css("background-image", image );
+            tiles.attr("value" , letters[i].value);
+            $(id).append(tiles);
+        }
+
+    }
+
+
+
+    // reRackLetter();
+
+
+
+
+
+    //NOTE:
+    // getRandomIndex should be makine care of the valid index. 
+    // Because some letter may run out, if it quantity is 0 
+    // meaning no more tiles of that letter. 
+    // need to generate a new index.
+    // if the make letter is running low in the bag, 
+    // that mean we may need by hang up in the getRandomIndex() since 
+    // this function return only the valid index. 
+    function getRandomIndex() {
+        return parseInt(Math.random() * 24 % 24);
     }
 
 
 
 
+    $(".tiles").draggable({
+        snap: ".snap",
+        snapMode: "inner"
+    });
 
-    $(".tiles").draggable({snap: ".snap", snapMode: "inner"});
+
+
+
+
+
+
+
 });
