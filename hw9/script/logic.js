c498aa4ce69e9697605af962a1c56da0b73e0dbd
Sokthai <sokthaitang@gmail.com>
2018-12-14 10:48:18 -0500

    fixing droppable disable logic file in drop option of droppable

--- hw9/script/logic.js
+++ hw9/script/logic.js
@@ -37,11 +37,35 @@ $(function () {
     });
 
     let value;
-    
+    let firstTile = true;
     //make tiles draggable
     $(".tiles").draggable({ //https://jqueryui.com/draggable/
         snap: ".snap",
         snapMode: "inner",
+        revert: function (obj) {
+            if (putBack) {
+                return false; //allow to put back to stand
+            }
+            if (gameStart) {
+
+                if (firstTile) {
+                    firstTile = false;
+                    $(this).draggable("disable");
+                    return false; //first tile always put in the center/star tile
+                } else {
+                    if (adjacentTile) {
+                        return false; // no revert
+                    } else {
+                        console.log("The letter must be next to each other");
+                        return true;
+                    }
+                }
+
+            } else {
+                alert("Please start the game from the star tile");
+                return true; //revert
+            }
+        },
         drag: function () {
             // console.log((this));
             // console.log($(this));
@@ -49,25 +73,41 @@ $(function () {
             // console.log(value);
         }
     });
-
+    let gameStart = false; //check if player play the star tile first
+    let adjacentTile = false; // check if player play tile next to the other tile
+    let putBack = false; // allow play to put letter back to the stand
     $(".snap").droppable({ //position the tile to the center of the block
+        out: function () {
+            $(this).droppable("option", "disabled", false);
+            console.log('im her');
+        },
         drop: function (event, ui) {
             let id = $(this).attr("id");
-            let cls = $(this).attr("class").slice(9, 11);
+            let cls = $(this).attr("class");
             let star = $("#h7").attr("value"); //check if the star tile is already play
+            console.log(id);
 
-            
-            
+            if (cls.slice(5, 16) === "letterStand") {
+                putBack = true;
+            } else {
+                putBack = false;
+            }
+
+
+
+
+
             if (id !== "h7" && star === undefined) {
-                alert("Please play the star first");
-            }else{
+                gameStart = false;
+                // $(".snap").droppable("disable");
+                // alert("Please play the star first");
+            } else {
 
-                console.log(star);
-                //console.log(cls);
+                gameStart = true;
 
                 $("#" + id).attr("value", value); //set the value to dropped element
                 checkDictoinary(id, value); //checking valid words from dictionary
-                calculatePlayScore(value, cls); //calculate the score each time user play (not total score)
+                calculatePlayScore(value, cls.slice(9, 11)); //calculate the score each time user play (not total score)
 
                 let sindex;
                 for (let i = 0; i < letters.length; i++) {
@@ -79,6 +119,7 @@ $(function () {
 
                 letters.splice(sindex, 1); //remove letter from the rack array after play
             }
+
             ui.draggable.position({ //https://api.jqueryui.com/position/
                 my: "center",
                 at: "center",
@@ -87,17 +128,27 @@ $(function () {
                     $(this).animate(pos, 20, "linear");
                 }
             })
+            // $(this).droppable("option", "disabled", true);
+            console.log("drop id " + id);
+            $("#" + id).droppable("disable");
         }
     })
 
     function checkDictoinary(id, value) {
         let hw = getWords(id, value); //getting horizonal words
         let vw = getWords(id, value, false); //getting vertical words
-
+        if (hw.length > 1 || vw.length > 1) {
+            adjacentTile = true; //if player put letter tile next to the other tile, then ok to play
+        } else {
+            adjacentTile = false;
+        }
+        console.log("hw is " + hw);
+        console.log("vw is " + vw);
     }
 
-    
+
     let totalGameScore, totalPlayScore = 0;
+
     function calculatePlayScore(value, cls) {
 
         let index = parseInt(value.charCodeAt(0)) - 65; //calculate the index of json
